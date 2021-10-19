67e76389844eed635f798a00e12665f91938a0ad
WebSaketh <saketh.kotagiri@stonybrookk.edu>
2021-10-19 02:46:38 -0400

    feat(Workspace): workspace renders items on refresh by parsing url

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -1,7 +1,7 @@
-import { useContext } from 'react'
-import { useHistory } from 'react-router-dom'
-import Top5Item from './Top5Item.js'
-import { GlobalStoreContext } from '../store'
+import { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import Top5Item from "./Top5Item.js";
+import { GlobalStoreContext } from "../store";
 /*
     This React component lets us edit a loaded list, which only
     happens when we are on the proper route.
@@ -9,39 +9,45 @@ import { GlobalStoreContext } from '../store'
     @author McKilla Gorilla
 */
 function Workspace() {
-    const { store } = useContext(GlobalStoreContext);
-    store.history = useHistory();
+  const { store } = useContext(GlobalStoreContext);
+  store.history = useHistory();
 
-    let editItems = "";
-    if (store.currentList) {
-        editItems = 
-            <div id="edit-items">
-                {
-                    store.currentList.items.map((item, index) => (
-                        <Top5Item 
-                            id={'top5-item-' + (index+1)}
-                            key={'top5-item-' + (index+1)}
-                            text={item}
-                            index={index} 
-                        />
-                    ))
-                }
-            </div>;
+  useEffect(() => {
+    if (store.currentList === null) {
+      store.setCurrentList(
+        window.location.href.substring("http://localhost:3000/top5list/".length)
+      );
     }
-    return (
-        <div id="top5-workspace">
-            <div id="workspace-edit">
-                <div id="edit-numbering">
-                    <div className="item-number">1.</div>
-                    <div className="item-number">2.</div>
-                    <div className="item-number">3.</div>
-                    <div className="item-number">4.</div>
-                    <div className="item-number">5.</div>
-                </div>
-                {editItems}
-            </div>
+  });
+  let editItems = "";
+  if (store.currentList) {
+    editItems = (
+      <div id="edit-items">
+        {store.currentList.items.map((item, index) => (
+          <Top5Item
+            id={"top5-item-" + (index + 1)}
+            key={"top5-item-" + (index + 1)}
+            text={item}
+            index={index}
+          />
+        ))}
+      </div>
+    );
+  }
+  return (
+    <div id="top5-workspace">
+      <div id="workspace-edit">
+        <div id="edit-numbering">
+          <div className="item-number">1.</div>
+          <div className="item-number">2.</div>
+          <div className="item-number">3.</div>
+          <div className="item-number">4.</div>
+          <div className="item-number">5.</div>
         </div>
-    )
+        {editItems}
+      </div>
+    </div>
+  );
 }
 
 export default Workspace;
